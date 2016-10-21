8fb98c8bc2e244cb8e05f7e85279267c88d703a7
ylee <ylee457@gmail.com>
2016-10-20 23:25:22 -0700

    no message

--- egauge/egauge_htp_go.rb
+++ egauge/egauge_htp_go.rb
@@ -64,32 +64,6 @@ end
 
 
 
-
-def get_google_map_data
-
-  begin
-
-  	eg = EguageHtp.new('maps.googleapis.com')
-  	query_param = {address: '11234 Broadway, New York, NY', sensor: 'false'}
-  	response = eg.get_test_data(query_param)
-
-    puts "\nResponse is: #{response}\n"
-
-    hash_xml = Crack::XML.parse(response.body)
-  
-    # pretty print to screen
-    puts "\nPretty print the output\n"
-    puts JSON.pretty_generate(hash_xml)
-    
-
-  rescue => e
-  	puts "\nError! #{e}\n"
-      
-  end
-
-end
-
-
 # to run: ruby egauge_htp_go.rb data_save/i-10-20-16pm.xml 
 def retrieve_xml_structure
  
